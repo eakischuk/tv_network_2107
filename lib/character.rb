c7d98f083af9b3a424020519ca81e26ca4403eb5
Erika Kischuk <erika.kischuk@gmail.com>
2021-07-21 09:11:32 -0600

    Add character class and attributes with tests

---
+++ lib/character.rb
@@ -0,0 +1,10 @@
+class Character
+  attr_reader :name, :actor, :salary
+
+  def initialize(details)
+    @name = details[:name]
+    @actor = details[:actor]
+    @salary = details[:salary]
+  end
+
+end
