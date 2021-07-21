3df630e02a3dadc6c149f5fbd6a164f2ba7255d5
Erika Kischuk <erika.kischuk@gmail.com>
2021-07-21 09:43:54 -0600

    Add add shows method and main characters method

--- lib/network.rb
+++ lib/network.rb
@@ -1,8 +1,20 @@
 class Network
   attr_reader :name, :shows
-  
+
   def initialize(name)
     @name = name
     @shows = []
+  end
+
+  def add_show(show)
+    @shows << show
+  end
+
+  def main_characters
+    main_characters = []
+    @shows.each do |show|
+      main_characters << show.characters
+    end
+    main_characters.flatten
   end
 end
